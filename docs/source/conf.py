a396dddd7c17501444fad1dbf1a1b0ef097a05fb
michaelf12095 <michaelf12095@outlook.com>
2026-06-05 16:48:12 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'Orbit Visualizer'
 copyright = '2026, Michael Ferrigno'
